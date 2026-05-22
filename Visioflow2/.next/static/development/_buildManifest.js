2f492831d7310ceeeaff3783a475cc86f6e1eebb
Yanis Boudadour <yns.boudadour@gmail.com>
2026-05-22 19:59:17 +0200

    Nettoyage : retrait du dossier .next de Git

--- Visioflow2/.next/static/development/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[{has:void 0,source:"\u002F.well-known\u002Fapple-developer-merchantid-domain-association",destination:"\u002Fapi\u002Fapple-pay-verify"}],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/dashboard":["static\u002Fchunks\u002Fpages\u002Fdashboard.js"],"/login-admin":["static\u002Fchunks\u002Fpages\u002Flogin-admin.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fdashboard","\u002Flogin-admin"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[{has:void 0,source:"\u002F.well-known\u002Fapple-developer-merchantid-domain-association",destination:"\u002Fapi\u002Fapple-pay-verify"}],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 46ff491 (maj)
